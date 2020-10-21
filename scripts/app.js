1dbcc15e5d1e100fedb1fcbbef98fe20a7e36536
min <minhazur9@gmail.com>
2020-10-21 01:34:33 -0400

    Links in the nav light up as you enter the sections

--- scripts/app.js
+++ scripts/app.js
@@ -1,16 +1,21 @@
 // Event Listeners
 
-$('.nav-item').on('click', function() {
+$('.nav-item').on('click', function () {
     const section = $(`.${$(this).attr('id')}`)
     let scroll;
-    if(!section.offset()) scroll = 250;
+    if (!section.offset()) scroll = 250;
     else scroll = section.offset().top;
-    $('html, body').animate({scrollTop: scroll-250},500)
+    $('html, body').animate({ scrollTop: scroll - 250 }, 500)
     $('.nav-item').removeClass('active')
-    $(this).addClass('active')   
-}) 
+    $(this).addClass('active')
+})
 
-$(window).on('scroll', function() {
-    const body = $('html')
-    console.log(body.offset())
+$(window).on('scroll', function () {
+    $('.nav-item').removeClass('active')
+    if (window.pageYOffset >= $('.contact').offset().top - 350) $('#contact').addClass('active')
+    else if (window.pageYOffset >= $('.projects').offset().top - 350) $('#projects').addClass('active')
+    else if (window.pageYOffset >= $('.skills').offset().top - 350) $('#skills').addClass('active')
+    else if (window.pageYOffset >= $('.about').offset().top - 350) $('#about').addClass('active')
+    else $('#home').addClass('active')
+    
 })
